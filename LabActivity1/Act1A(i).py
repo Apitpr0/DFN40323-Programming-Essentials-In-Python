e891451422829e0fe87e1fb951668ba5888f7707
Apitpr0 <apitpro123@gmail.com>
2022-08-23 12:59:35 +0800

    half done lab activity 1

---
+++ LabActivity1/Act1A(i).py
@@ -0,0 +1,18 @@
+x=str("Hello World")
+x=int(20)
+x=float(20.5)
+x=5
+print(type(x))
+
+#Legal Variable Name
+myvar="John"
+my_var="John"
+_my_var="John"
+myVar="John"
+MYVAR="John"
+myvar2="John"
+
+#Illegal Variable Name
+2myvar="John"
+my-var="John"
+my var="John"
---
+++ LabActivity1/Act1A(ii).py
@@ -0,0 +1,22 @@
+#variables
+prog="DDT4"
+noOFStudent=80
+print(prog)
+print(noOFStudent)
+
+#change type of variables
+x=4 # x is of type int
+x="Sally" # x is now of type str
+print(x)
+
+#assign values to multiple variables in one line
+f1,f2,f3="Orange","Banana","Cherry"
+print(f1)
+print(f2)
+print(f3)
+
+#Assign the same value to multiple variables in one line
+f1,f2,f3="apple"
+print(f1)
+print(f2)
+print(f3)
