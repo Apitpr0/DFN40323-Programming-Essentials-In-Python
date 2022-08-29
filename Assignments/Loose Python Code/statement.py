270f89ec0cfeb0f83966856b5a40c351e95ef9a2
Apitpr0 <apitpro123@gmail.com>
2022-08-29 09:47:38 +0800

    if statement play

---
+++ Assignments/Loose Python Code/statement.py
@@ -0,0 +1,25 @@
+'''
+if statement
+a=33
+b=200
+if(b>a):
+    print("B is greater then A")
+'''
+'''
+else statement 
+a=200
+b=33
+if(b>a):
+    print("B is greater then A")
+else:
+    print("A is greater then B")
+'''
+
+a=33
+b=33
+if(b>a):
+    print("B is greater then A")
+elif a==b:
+    print("A and B are equal")
+else:
+    print("A is less then B")
