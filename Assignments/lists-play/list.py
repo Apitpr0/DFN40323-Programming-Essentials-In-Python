6fd804bbf4aa4282c98f99c696119aa53c814b54
Apitpr0 <apitpro123@gmail.com>
2022-10-03 09:56:50 +0800

    differentiate tuple,list and dict

--- Assignments/lists-play/list.py
+++ Assignments/lists-play/list.py
@@ -1,3 +1,29 @@
+'''
 Stationery = ['Pencil', 'Eraser', 'Ruler']
 x = Stationery.count("Eraser")
 print(x)
+'''
+'''
+listname=["yourdata","Yourdata","YOURDATA"]
+print(nameofthelist)
+'''
+#Syntax for creating list
+'''
+namelist=(data)
+'''
+'''
+entah = [True, False, True]
+print(entah)
+'''
+tupleni = (False,False,True)
+print(tupleni)
+
+dictionaryini={
+    "nasi":"lemak", #String
+    "sambal":"petai",#String
+    "harga":5, #int
+    "harga":80, #int
+    "lauk":["paru","kerang"],  #List
+    "pedas":True    #Boolean
+}
+print(dictionaryini)
